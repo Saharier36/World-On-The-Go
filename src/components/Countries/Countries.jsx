3e0530068a64c726cf6a7a00767f176e8c94fd8a
Saharier Omi <gsaharier761@gmail.com>
2026-03-24 16:53:54 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>In The Countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
+
