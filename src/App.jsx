29a4c2b6b18c4b8cec33ce6129a2562bb76e6b68
Saharier Omi <gsaharier761@gmail.com>
2026-03-24 18:11:52 +0600

    Integrates data fetching with Suspense for countries list
    Enables asynchronous loading of country data using React Suspense and the new use() hook, improving UI responsiveness and data management. Prepares for further country detail enhancements by scaffolding a new country component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import './App.css'
-import Countries from './components/countries/countries'
+import { Suspense } from "react";
+import "./App.css";
+import Countries from "./components/countries/countries";
+
+const countriesPromise = fetch(
+  "https://openapi.programming-hero.com/api/all",
+).then((res) => res.json());
 
 function App() {
-
-  
   return (
     <>
-  
-    <Countries>
-      
-    </Countries>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { use } from 'react';
 
-const Countries = () => {
-    return (
-        <div>
-            <h1>In The Countries</h1>
-        </div>
-    );
+
+const Countries = ({ countriesPromise }) => {
+    const countriesData = use(countriesPromise);
+    const countries = countriesData.countries
+    console.log(countries);
+  return (
+    <div>
+      <h1>In The Countries: {countries.length}</h1>
+    </div>
+  );
 };
 
 export default Countries;
