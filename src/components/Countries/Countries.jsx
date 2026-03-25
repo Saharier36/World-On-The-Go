c2967eb18bb7405572c85985ea7b3c26b05df1ea
Saharier Omi <gsaharier761@gmail.com>
2026-03-25 12:31:46 +0600

    added countries and country data

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { use } from 'react';
-
+import React from "react";
+import { use } from "react";
+import Country from "../Country/Country";
 
 const Countries = ({ countriesPromise }) => {
-    const countriesData = use(countriesPromise);
-    const countries = countriesData.countries
-    console.log(countries);
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+
   return (
     <div>
       <h1>In The Countries: {countries.length}</h1>
+      {countries.map((country) => (
+        <Country key={country.cca3.cca3} country={country}></Country>
+      ))}
     </div>
   );
 };
 
 export default Countries;
-
